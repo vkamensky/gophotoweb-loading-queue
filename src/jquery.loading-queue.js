3cfe330e71d4821ec08d71c51d1d7ed1cdfe4456
Valery Kamensky <valery.kamensky@cliqus.com>
2013-04-12 17:26:19 +0300

    competitive intialization bug has been fixed

--- src/jquery.loading-queue.js
+++ src/jquery.loading-queue.js
@@ -2,84 +2,91 @@
 	
 	var pluginName = 'loadingQueue';
 	
-	var queue = new Array();
-	
-	var loading = false;
-	
-	var buildUrl = function(image, options) {
+	var LoadingQueue = function() {
 		
-		var url = null;
+		var self = this;
 		
-		if(options) {
+		this.queue = new Array();
+		
+		this.loading = false;
+		
+		this.buildUrl = function(image, options) {
 			
-			$.each(options, function(prefix, size) {
+			var url = null;
+			
+			if(options) {
 				
-				if(!url || screen.width > size.width && screen.height > size.width) {
-					url = image.data('basePath') + prefix + image.data('fileName');
-				}
-			});
+				$.each(options, function(prefix, size) {
+					
+					if(!url || screen.width > size.width && screen.height > size.width) {
+						url = image.data('basePath') + prefix + image.data('fileName');
+					}
+				});
+				
+			} else if(image.data('src')) {
+				
+				url = image.data('src');
 			
-		} else if(image.data('src')) {
-			
-			url = image.data('src');
-		
-		} else {
-			
-			url = image.data('basePath') + image.data('fileName');
-		}
-		
-		return url;
-	};
-	
-	var dequeue = function () {
-		
-		var item = queue.shift();
-		
-		if(item) {
-			
-			loading = true;
-			
-			if(!item.image.is('img')) {
-				dequeue();
-				return;
+			} else {
+				
+				url = image.data('basePath') + image.data('fileName');
 			}
 			
-			var src = buildUrl(item.image, item.options);
-			
-			item.image
-				.on('load', function() {
-					
-					item.image.trigger('loaded');
-					
-					dequeue();
-				})
-				.on('error', function() {
-					
-					dequeue();
-				})
-				.attr('src', src);
-			
-		} else {
-			loading = false;
-		}
-	};
+			return url;
+		};
 		
-	var enqueue = function(image, options) {
-		
-		queue.push({
-			image: $(image),
-			options: options
-		});				
-	};
+		this.dequeue = function () {
+			
+			var item = this.queue.shift();
+			
+			if(item) {
+				
+				this.loading = true;
+				
+				if(!item.image.is('img')) {
+					this.dequeue();
+					return;
+				}
+				
+				var src = this.buildUrl(item.image, item.options);
+				
+				item.image
+					.on('load', function() {
+											
+						item.image.trigger('loaded');
+						
+						self.dequeue();
+					})
+					.on('error', function() {
+						
+						self.dequeue();
+					})
+					.attr('src', src);
+				
+			} else {
+				this.loading = false;
+			}
+		};
+			
+		this.enqueue = function(image, options) {
+			
+			this.queue.push({
+				image: $(image),
+				options: options
+			});				
+		};
+	};	
 		
 	$.fn[pluginName] = function (options) {
 		
+		var q = new LoadingQueue();
+		
 		$(this).each(function() {
 			
-			enqueue($(this), options);
+			q.enqueue($(this), options);
 			
-			if(!loading) {
-				dequeue();
+			if(!q.loading) {
+				q.dequeue();
 			}
 			
 		});		
